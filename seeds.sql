0de97437f339922b1db909a80e68131bda081594
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-13 10:26:14 -0500

    changes to seeds

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,2 @@
 INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES("Crest", "Home Goods", 4.50, 40), (?, ?, ?, ?), (?, ?, ?, ?)
--- ADD DUMMY DATA ABOVE^^
+VALUES("Star Wars Movie", "Technology", 5.00, 40), ("jeans", "clothing", "10.75", "75"), ("Pizza", "food", "3.20", "49")

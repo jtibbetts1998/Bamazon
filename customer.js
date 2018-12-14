dce8bab0cd3ab33a453c3a71eb3cb89d6bf792b9
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-14 15:01:56 -0500

    setting up table and starting second function

--- customer.js
+++ customer.js
@@ -1,53 +1,84 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("cli-table");
+
+var newTable = new table({
+    head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'],
+    colWidths: [20, 20, 20, 20, 20]
+});
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "bamazon_DB"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon_DB"
 });
 
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 });
 
 function start() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        var initialArray = [];
+        var dataArray = [];
+        for (i = 0; i < res.length; i++) {
+            initialArray.push(res[i].item_id)
+            initialArray.push(res[i].product_name)
+            initialArray.push(res[i].department_name)
+            initialArray.push(res[i].price)
+            initialArray.push(res[i].stock_quantity)
+            dataArray.push(initialArray)
+            initialArray = []
+            newTable.push(dataArray[i])
+        }
+        var blah = newTable.toString();
+        console.log(blah);
         postOrBid();
         //call postOrbid function next()
     })
 }
 // function which prompts the user for what action they should take
 function postOrBid() {
-  inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "rawlist",
-      message: "What would you like to do??",
-      choices: ["add", "post"]
-    })
-    .then(function(answer) {
-        console.log(answer);
-      // based on their answer, either call the Add or the View functions
-      if (answer.postOrBid === "add") {
-          console.log(answer.postOrBid);
-        addStore();
-      }
-      //else {
-    //     ViewStore();
-    //   }
-    });
+    inquirer
+        .prompt({
+            name: "postOrBid",
+            type: "input",
+            message: "Select the ID of the product you'd like to buy: "
+        })
+        .then(function (answer) {
+            //refer to line 111-116 greatbay.js
+            input = parseInt(answer.postOrBid)
+            console.log(input);
+            connection.query("SELECT * FROM products WHERE item_id = " + answer.postOrBid, function (err, res) {
+                if (err) throw err;
+                console.log(res);
+                addStore();
+            })
+            //select the id from the database and display sql SELECT statement
+        })
 }
 
 function addStore() {
-    console.log("add store");
+    inquirer
+        .prompt({
+            name: "addStore",
+            type: "input",
+            message: "How many units would you like to buy?"
+        })
+        .then(function (answer) {
+            connection.query("UPDATE products SET stock_quantity = ")
+            console.log(answer);
+            // based on their answer, either call the Add or the View functions
+            if (answer.addStore === "post") {
+                console.log(answer.addStore);
+            }
+        });
 }
--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO products(product_name, department_name, price, stock_quantity) 
-VALUES("Star Wars Movie", "electronics", 5.00, 40), ("jeans", "clothing", 0.75, 20), ("Pizza", "food", 3.20, 49), 
-("watch", "mens", 35.95, 10), ("pokemon", "toy", 3.99, 15), ("shoes", "footwear", 45, 30), ("floss", "personal care", 2.35, 13),
-("dog bowl", "pets", 5.95, 7), ("basketball", "sports", 7.89, 14), ("pencil", "supplies", 1.98, 10)
+INSERT INTO products(item_id, product_name, department_name, price, stock_quantity) 
+VALUES(1, "Star Wars Movie", "electronics", 5, 40), (2, "jeans", "clothing", 1, 20), (3, "Pizza", "food", 3, 49), 
+(4, "watch", "mens", 35, 10), (5, "pokemon", "toy", 3, 15), (6, "shoes", "footwear", 45, 30), (7, "floss", "personal care", 2, 13),
+(8, "dog bowl", "pets", 5, 7), (9, "basketball", "sports", 7, 14), (10, "pencil", "supplies", 1, 10)

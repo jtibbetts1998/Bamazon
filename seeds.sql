cfb97befbd43a0ee6d8cee77fd22c03b0d3af990
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-14 15:23:36 -0500

    updating seeds sql

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO products(item_id, product_name, department_name, price, stock_quantity) 
-VALUES(1, "Star Wars Movie", "electronics", 5, 40), (2, "jeans", "clothing", 1, 20), (3, "Pizza", "food", 3, 49), 
-(4, "watch", "mens", 35, 10), (5, "pokemon", "toy", 3, 15), (6, "shoes", "footwear", 45, 30), (7, "floss", "personal care", 2, 13),
-(8, "dog bowl", "pets", 5, 7), (9, "basketball", "sports", 7, 14), (10, "pencil", "supplies", 1, 10)
+VALUES(1, "Star Wars Movie", "electronics", 5, 40), (2, "jeans", "clothing", 0.75, 20), (3, "Pizza", "food", 3.2, 49), 
+(4, "watch", "mens", 35.95, 10), (5, "pokemon", "toy", 3.99, 15), (6, "shoes", "footwear", 45, 30), (7, "floss", "personal care", 2.35, 13),
+(8, "dog bowl", "pets", 5.95, 7), (9, "basketball", "sports", 7.89, 14), (10, "pencil", "supplies", 1.98, 10)
